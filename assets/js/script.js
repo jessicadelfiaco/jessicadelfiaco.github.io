4259c5bc2edb4556604e1b722ab35e0bd1e0b2d7
root <root@DESKTOP-H36QD8E.localdomain>
2020-08-16 19:43:32 -0300

    Ajustes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 function mostrar(e) {
-    if (e.classList.contains("fa-moon-o")) { //se tem lua
+    // Esta em lightmode. Alterna para darkmode
+    if (e.classList.contains("fa-moon-o")) { 
         e.classList.remove("fa-moon-o"); //remove classe icone lua
         e.classList.add("fa-sun-o"); //coloca classedo sol
         e.style.color = "rgb(225, 225, 0)";
@@ -7,22 +8,18 @@ function mostrar(e) {
         document.querySelector('#userName').style.color = '#fff';
 
         let links = document.querySelectorAll('.link');
-        // Pega apenas o primeiro do array
-        links[0].style.filter = 'grayscale(100%)';
-        // pega todos
+        // altera os links para cinza
         for (let i = 0; i < links.length; i++) {
-            links[i].style.filter = 'grayscale(100%)';
+          links[i].style.filter = 'grayscale(100%)';
         }
 
         let circulos = document.querySelectorAll('.circulo');
-        // Pega apenas o primeiro do array
-        circulos[0].style.filter = 'grayscale(100%)';
-        // pega todos
+        // altera os ciculos para cinza
         for (let i = 0; i < circulos.length; i++) {
             circulos[i].style.filter = 'grayscale(100%)';
         }
 
-    } else { //senão
+    } else { // Esta em darkmode. Alterna para lightmode
         e.classList.remove("fa-sun-o"); //remove classe icone lua
         e.classList.add("fa-moon-o"); //coloca classedo sol
         e.style.color = "#585858";
@@ -30,17 +27,13 @@ function mostrar(e) {
         document.querySelector('#userName').style.color = 'rgb(99, 99, 99)';
 
         let links = document.querySelectorAll('.link');
-        // Pega apenas o primeiro do array
-        links[0].style.filter = 'grayscale(0%)';
-        // pega todos
+        // remove o cinza dos links
         for (let i = 0; i < links.length; i++) {
             links[i].style.filter = 'grayscale(0%)';
         }
 
         let circulos = document.querySelectorAll('.circulo');
-        // Pega apenas o primeiro do array
-        circulos[0].style.filter = 'grayscale(0%)';
-        // pega todos
+        // remove o cinza dos circulos
         for (let i = 0; i < circulos.length; i++) {
             circulos[i].style.filter = 'grayscale(0%)';
         }
